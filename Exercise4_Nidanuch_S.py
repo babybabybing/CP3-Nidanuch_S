475c1ee8511ddecc16785a31bab638f948e2cf6b
babybabybing <babybabybing@hotmail.com>
2021-09-22 21:40:48 +1000

    Exercise4

---
+++ Exercise4_Nidanuch_S.py
@@ -0,0 +1,11 @@
+FE = 80
+GB = 79.5
+Intro = 65.5
+CP = 70.5
+
+print ("Your Score")
+print ("---------------------------------------")
+print ("Foundation English               :", FE)
+print ("General Business                 :", GB)
+print ("Introduction to Computer Systems :", Intro)
+print ("Computer Programming             :", CP)
