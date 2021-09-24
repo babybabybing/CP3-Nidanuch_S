a8772fec6136cbe37ac8e4cbdd6a937036736c41
babybabybing <babybabybing@hotmail.com>
2021-09-24 23:19:26 +1000

    Ex5.1

---
+++ Exercise5_1_Nidanuch_S.py
@@ -0,0 +1,8 @@
+x= float(input("Number1:"))
+y= float(input("Number2:"))
+
+print(str(x),"+",str(y),"=", x+y)
+print(str(x),"-",str(y),"=", x-y)
+print(str(x),"*",str(y),"=", x*y)
+print(str(x),"/",str(y),"=", x/y)
+
